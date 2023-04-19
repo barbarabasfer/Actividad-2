54da2badd4cd2bc42ad81398553f9a4d7657d7f3
Barbara <barbara.basfer@educa.jcyl.es>
2023-04-20 00:15:11 +0200

    Parte 2

--- Actividad2.java
+++ Actividad2.java
@@ -11,6 +11,7 @@ public class Prueba {
 		System.out.print("Escribe un número: ");
 		int entero = sc.nextInt();
 		
+		// Bucle que se inicializa en 1 
 		for (int i = 1; i < 10; i++) {
 			
 			// Muestra la tabla del número que indiquemos
